5eb8e3dac3ee4729b2a47a274b99d7763fb656eb
Jenny <jenny@Jennys-MacBook-Pro.local>
2022-04-08 14:05:23 -0600

    pushing to github

---
+++ wordfinder.py
@@ -0,0 +1,18 @@
+"""Word Finder: finds random words from a dictionary."""
+import random
+
+class WordFinder:
+    def __init__(self, path):
+        file = open(path)
+        self.words = self.parse(file)
+        print(f'{len(self.words)} words read')
+    def parse(self, file):
+        return [w.strip() for w in file]
+    def random(self):
+        return random.choice(self.words)
+
+class SpecialWordFinder(WordFinder):
+    def parse(self, file):
+        return [w.strip() for w in file if w.strip() and not w.startswith("#")]
+
+        
